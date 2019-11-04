59319ea6d130770d7ecf169cacfcc73288419ba8
Paul Coltofeanu <paulcolto@gmail.com>
2019-11-04 15:03:31 +0100

    authentication

--- resources/js/App/Components/App.jsx
+++ resources/js/App/Components/App.jsx
@@ -12,8 +12,33 @@ export default class App extends React.Component {
         }
     }
 
-    componentDidMount = () => {
+    getToken = () => {
+        return window.localStorage.getItem('_token');
+    }
 
+    setToken = (token) => {
+        window.localStorage.setItem('_token', token);
+    }
+
+    componentDidMount = () => {
+        if (null === this.getToken()) {
+            this.setState({
+                logged_in: false
+            })
+        } else {
+            this.setState({
+                logged_in: true
+            })
+        }
+    }
+
+    onLoginSuccess = (token) => {
+        this.setToken(token);
+
+        this.setState({
+            logged_in: true,
+            token: token
+        })
     }
 
     render() {
@@ -23,7 +48,7 @@ export default class App extends React.Component {
             if (this.state.logged_in) {
                 content = <PersonList />;
             } else {
-                content = <LoginForm />;
+                content = <LoginForm onLoginSuccess={ this.onLoginSuccess }/>;
             }
         }
 
